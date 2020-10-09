04a449c2865957cc52f0b1ed32aacd702c1e3971
Anant Rawat <anant1995rawat@gmail.com>
2020-10-09 12:02:28 +0530

    Async functionality with react-redux

--- src/App.js
+++ src/App.js
@@ -6,16 +6,18 @@ import HooksCakeContainer from "./components/HooksCakeContainer";
 import IceCreamContainer from "./components/IceCreamContainer";
 import NewCakeContainer from "./components/NewCakeContainer";
 import ItemContainer from "./components/ItemContainer";
+import UserContainer from "./components/UserContainer";
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <ItemContainer />
+        {/* <ItemContainer />
         <ItemContainer cake />
         <HooksCakeContainer />
         <CakeContainer />
         <IceCreamContainer />
-        <NewCakeContainer />
+        <NewCakeContainer /> */}
+        <UserContainer />
       </div>
     </Provider>
   );
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import cakeReducer from "./cake/cakeReducer";
 import iceCreamReducers from "./iceCream/iceCreamReducers";
+import reducer from "./user/userReducer";
 
 const rootReducer = combineReducers({
   cake: cakeReducer,
   iceCream: iceCreamReducers,
+  user: reducer,
 });
 
 export default rootReducer;
